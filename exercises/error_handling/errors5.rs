5958db88c54089bd9c28a0701fd924836411c931
Your Name <you@example.com>
2024-10-19 01:45:10 +0800

    solve ...60

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -22,14 +22,14 @@
 // Execute `rustlings hint errors5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
-// TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+
+fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
@@ -57,7 +57,7 @@ impl PositiveNonzeroInteger {
     }
 }
 
-// This is required so that `CreationError` can implement `error::Error`.
+
 impl fmt::Display for CreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
@@ -67,5 +67,6 @@ impl fmt::Display for CreationError {
         f.write_str(description)
     }
 }
+
 
 impl error::Error for CreationError {}
